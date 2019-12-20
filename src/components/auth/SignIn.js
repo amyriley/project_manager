14dfcd9ead4389407196d0641aba3172a9ea7835
amrmynd <ariley.amy@gmail.com>
2019-12-20 15:27:46 +0100

    adding navlinks and styling

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class SignIn extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
     }
 
     handleChange = (e) => {
